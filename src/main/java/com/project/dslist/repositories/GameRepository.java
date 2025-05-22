7216c6315a75f5a09aad845de1ad9369c0a2ce30
caiotelesz <caioteles1267@gmail.com>
2025-05-22 17:59:52 -0300

    feat: create GameMinDTO, GameRepository, GameService, and GameController

---
+++ src/main/java/com/project/dslist/repositories/GameRepository.java
@@ -0,0 +1,9 @@
+package com.project.dslist.repositories;
+
+import com.project.dslist.entities.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GameRepository extends JpaRepository<Game, Long> {
+}
