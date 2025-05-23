0f94ba562698ceb76ce19a5074ca1d66f12b5375
caiotelesz <caioteles1267@gmail.com>
2025-05-23 11:52:27 -0300

    feat(list): adicionar controller, service, repository e DTO para busca de listas

---
+++ src/main/java/com/project/dslist/repositories/GameListRepository.java
@@ -0,0 +1,9 @@
+package com.project.dslist.repositories;
+
+import com.project.dslist.entities.GameList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GameListRepository extends JpaRepository<GameList, Long> {
+}
