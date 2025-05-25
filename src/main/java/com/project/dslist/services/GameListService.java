09d6264e05f27bbd92d1c065f0da713f07de26db
caiotelesz <caioteles1267@gmail.com>
2025-05-25 12:05:03 -0300

    feat: add draggable reordering to games list

--- src/main/java/com/project/dslist/services/GameListService.java
+++ src/main/java/com/project/dslist/services/GameListService.java
@@ -2,7 +2,9 @@ package com.project.dslist.services;
 
 import com.project.dslist.dto.GameListDTO;
 import com.project.dslist.entities.GameList;
+import com.project.dslist.projections.GameMinProjection;
 import com.project.dslist.repositories.GameListRepository;
+import com.project.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +17,9 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll() {
         List<GameList> result = gameListRepository.findAll();
@@ -23,5 +28,20 @@ public class GameListService {
                 .map(x -> new GameListDTO(x))
                 .toList();
         return dto;
+    }
+
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+        for (int i = min; i <= max; i++) {
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
     }
 }
